0158ce95881e4907931b242cbf418071f0779349
Papichulo118 <temirkhan.seken@mail.ru>
2025-03-14 13:47:16 +0500

    ""

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/temirkhan/Desktop/streamlit_touristics/venv"
+set -gx VIRTUAL_ENV "/Users/temirkhan/Desktop/sayakhaaaaaaaaaaaaaaaaaaaaaaaaaat/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
